f2c3808e7a5b8d969d9d9607645d02f19489a9e2
JakeTB <jtilbybaxter@gmail.comgit config --global user.name Jakegit config --global user.email jtilbybaxter@gmail.com>
2020-01-20 10:22:07 +0000

    PRATING THAT THIS WORKS

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 const { DB_URL } = process.env;
-console.log(DB_URL);
-const ENV = process.env.NODE_ENV || "development";
 
+const ENV = process.env.NODE_ENV || "development";
+console.log(ENV, "<<<<<ENV");
 const baseConfig = {
   client: "pg",
   migrations: {
@@ -18,19 +18,20 @@ const customConfig = {
       database: "nc_news",
       user: "jake",
       password: "1234psql"
-    },
-
-    test: {
-      connection: {
-        database: "nc_news_test",
-        user: "jake",
-        password: "1234psql"
-      }
-    },
-    production: {
-      connection: `${DB_URL}?ssl=true`
     }
+  },
+  test: {
+    connection: {
+      database: "nc_news_test",
+      user: "jake",
+      password: "1234psql"
+    }
+  },
+  production: {
+    connection: `${DB_URL}?ssl=true`
   }
 };
+
+console.log(customConfig[ENV]);
 
 module.exports = { ...customConfig[ENV], ...baseConfig };
