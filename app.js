0aed3d4e361a4ca9fd6f373109670b484af1efee
JakeTB <jtilbybaxter@gmail.comgit config --global user.name Jakegit config --global user.email jtilbybaxter@gmail.com>
2020-01-20 14:15:00 +0000

    Fixed cors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const { apiRouter } = require("./routers/apiRouter");
 const { customErrors, psqlErrors } = require("./errors");
 const cors = require("cors");
-app.user(cors());
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 
