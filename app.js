8b1b69446a3c15363777eea343c4b480fccb6a65
JakeTB <jtilbybaxter@gmail.comgit config --global user.name Jakegit config --global user.email jtilbybaxter@gmail.com>
2020-01-20 14:10:53 +0000

    Added cors to back end

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const { apiRouter } = require("./routers/apiRouter");
 const { customErrors, psqlErrors } = require("./errors");
+const cors = require("cors");
+app.user(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 
